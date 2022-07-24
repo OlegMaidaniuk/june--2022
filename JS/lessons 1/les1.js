06d0918c727665deaf0ee25791d3d035c3c97305
OlegMaidaniuk <omaydanuk@gmail.com>
2022-07-24 21:14:13 +0300

    Initial commit

---
+++ JS/lessons 1/les1.js
@@ -0,0 +1,80 @@
+
+
+// console.log(fd)
+// console.log(ou)
+// console.log(om)
+// console.log(fg)
+// console.log(a)
+// console.log(b)
+// console.log(c)
+// console.log(d)
+// console.log(e)
+// console.log(f)
+// console.log(g)
+// console.log(h)
+// console.log(i)
+
+
+// let fd = 'hello';
+// let ou = 'owu';
+// let om = 'com';
+// let fg = 'ua';
+// let a = 1;
+// let b = '10';
+// let c = "-999";
+// let d = 123;
+// let e = 3.14;
+// let f = 2.7;
+// let g = 16;
+// let h = true;
+// let i = false;
+
+
+
+//     let fn = Oleg;
+//     let mn = Volodumyrovich;
+//     let ln = Maidaniuk;
+//     // let person = fn + '' + mn +  '' + ln;
+//     let person = '${fn} ${mn} ${ln}'
+//
+//
+//     console
+//         .log(person);
+//
+//
+//     let fn = promtp(message
+//     :
+//     'entr you first name'
+//     )
+//
+//
+//     let mn = promtp (message
+//     :
+//     'entr you midel name'
+//     )
+//
+//     let ln = promptp(message
+//     :
+//     'entr you last name name'
+//     )
+// let person = '${fn} ${mn} ${ln}'
+//     console.log(person)
+
+// let a = 100
+// let b = '100'
+// let c = true
+// console.log(typeof a)
+// console.log( typeof b )
+// console.log(typeof c )
+
+
+
+
+
+
+
+
+
+
+
+
